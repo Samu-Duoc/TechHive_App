cfa0211023cf7bde08e3cb6d41e99bfe19f9f8d8
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-11-21 23:07:18 -0300

    Feat: Se agreron nuevas iamgenes de productos

---
+++ TechHive_App_V2/app/src/main/java/com/example/techhive_app/ui/screen/SplashScreen.kt
@@ -0,0 +1,42 @@
+package com.example.techhive_app.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.techhive_app.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    onTimeout: () -> Unit
+) {
+    //Desde aqui carga al app
+    LaunchedEffect(Unit) {
+        delay(3000)
+        onTimeout()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFE7B62B)),
+        contentAlignment = Alignment.Center
+    ) {
+        //Logo de la imamgen
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo TechHive",
+            modifier = Modifier
+                .height(220.dp)
+                .width(220.dp)
+        )
+    }
+}
