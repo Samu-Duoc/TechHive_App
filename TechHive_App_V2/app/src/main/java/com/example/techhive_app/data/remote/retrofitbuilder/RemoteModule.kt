2fa54c9469b38d14dd460efbaf95bfb76da0a79c
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-12-14 22:39:54 -0300

    La aplicación se encuentra funcionando correctamente en ambas vistas (Administrador y Cliente). Cuenta con cuatro microservicios conectados, todos en funcionamiento. Se realizaron algunos tests. Actualmente existe un pequeño bug al editar el perfil y la única funcionalidad que no se logró completar fue la recuperación de contraseña.

--- TechHive_App_V2/app/src/main/java/com/example/techhive_app/data/remote/retrofitbuilder/RemoteModule.kt
+++ TechHive_App_V2/app/src/main/java/com/example/techhive_app/data/remote/retrofitbuilder/RemoteModule.kt
@@ -21,7 +21,7 @@ object RemoteModule {
 
     // Emulador Android hablando con tu PC (localhost:8081)
     // Cambia la IP según tu red local
-    private const val BASE_URL = "http://192.168.1.96:8081/"
+    private const val BASE_URL = "http://10.0.2.2:8081/"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -44,7 +44,7 @@ object RemoteModule {
 
 
     //PRODCUTOS
-    private const val PRODUCT_BASE_URL = "http://192.168.1.96:8082/"
+    private const val PRODUCT_BASE_URL = "http://10.0.2.2:8082/"
 
     val productApi: ProductApi by lazy {
         Retrofit.Builder()
@@ -55,7 +55,7 @@ object RemoteModule {
     }
 
     //CONTACTO
-    private const val CONTACT_BASE_URL = "http://192.168.1.96:8085/"
+    private const val CONTACT_BASE_URL = "http://10.0.2.2:8085/"
 
     val contactApi: ContactApi by lazy {
         Retrofit.Builder()
@@ -67,7 +67,7 @@ object RemoteModule {
     }
 
     //PEDIDOS
-    private const val PEDIDO_BASE_URL = "http://192.168.1.96:8084/"
+    private const val PEDIDO_BASE_URL = "http://10.0.2.2:8084/"
 
     val pedidoApi: PedidoApi by lazy {
         Retrofit.Builder()
